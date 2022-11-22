d2fec090ac825690cb373e4e43cacaad084f7c28
Kush Bhavsar <kushbhavsar393@gmail.com>
2022-11-22 19:22:23 +0530

    updated to 2022

---
+++ fish/js/index.js
@@ -0,0 +1,22 @@
+// let st = "Hello world" ;
+// let res = st.toLowerCase();
+// console.log(res);  
+
+// let a = ["hello" , "world"];
+// const  arr =(str) => str;
+// console.log (arr("Hello World"));
+
+// const arr = ["banana" , "apple", "graphes"] ;
+// const res = arr.map(r => r . length>5);
+// console.log(res);
+
+// const n = [1,2,3,4,5,6];
+// const v = n.map ( n=> n%2 ==0);
+// console.log(`even number are ${v}`); 
+
+// let i=1;
+// while(i<=100);
+// {
+//     console.log(i);
+//     i-i+1;
+// }
